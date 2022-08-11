3f84dc5c2dfc40f2aaac848ef93a4615ff8e97ad
Matias Schmidt <matias.schmidt@outlook.es>
2022-08-11 20:25:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React from 'react'; 
+import {useState} from 'react';
 import './App.css';
+import { BiSearch} from "react-icons/bi";
+import QRCode from "react-qr-code";
 
 function App() {
+    const [active, setActive] = useState(false)
+    const [name, setName] = useState('')
+    const [QR, setQR] = useState('https://github.com/mschmidt17')
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        if (active === true) {
+            setActive(false)
+        } else {
+            setActive(true)
+        }
+    }
+
+    const handleInputChange = (e) => {
+        e.preventDefault()
+        setName(e.target.value)                   
+    }
+
+    const handleGenerate = (e) => {
+        e.preventDefault()
+        setQR(name)
+        setName('')  
+
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className={`search${active ? '.active' : ''}`}>
+            <input 
+                value = {name}
+                type="text" 
+                className="input" 
+                placeholder="Generate QR..."
+                onChange={(e) => handleInputChange(e)}
+            />
+            <button className="btn" onClick={handleClick}> <BiSearch/> </button>
+        </div>
+
+        <div className='enter'>
+            <button className="btn-enter" onClick={handleGenerate}> GENERATE QR </button>
+        </div>
+
+        <div className='codigo'>
+            <QRCode value={QR} size={256} bgColor="#282c34" fgColor="#fff" level="H"/>
+        </div>
+        
     </div>
   );
 }
